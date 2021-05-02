274d62050b00cd8ce91a023e85193b57ec808d15
João Victor Batista <jvtexbat@hotmail.com>
2021-05-02 11:55:54 -0300

    sidebar adicionada

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/header/Header';
+import Sidebar from './components/sidebar/Sidebar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Sidebar />
+      <Header />   
     </div>
   );
 }
---
+++ front/src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import icon from '../../assets/icon_pomodoro.png';
+import './Sidebar.css';
+
+function Sidebar() {
+  return (
+    <section className="sidebar">
+        <div>Side</div>
+    </section>
+  );
+}
+
+export default Sidebar;
