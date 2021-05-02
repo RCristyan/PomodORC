6f1c65b7e155bd5467b4bff16273fbc965fddb00
thyago <thyagohenriquemoura@gmail.com>
2021-05-02 18:31:15 -0300

    adicionado circulo do timer

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from './components/header/Header';
 import Sidebar from './components/sidebar/Sidebar';
+import Timer from './components/timer/timer';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
-      <Header />   
+      <Timer/>  
+      <Header />
     </div>
   );
 }
---
+++ front/src/components/timer/timer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './timer.css';
+
+function Timer() {
+  return (
+    <div className="timer">
+    </div>
+  );
+}
+
+export default Timer;
