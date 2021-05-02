7dd5265aaba4f9aa6a5f998f668649578ab1427b
thyago <thyagohenriquemoura@gmail.com>
2021-05-02 19:07:05 -0300

    criado o timer estatico e estilizado

--- front/src/components/timer/timer.tsx
+++ front/src/components/timer/timer.tsx
@@ -4,6 +4,11 @@ import './timer.css';
 function Timer() {
   return (
     <div className="timer">
+        <div className="circle">
+            <div className="numbers">
+                <h1>00:00</h1>
+            </div>
+        </div>
     </div>
   );
 }
