7b501df26cf691e63fcc04d2e4802f10c9170f55
João Victor Batista <jvtexbat@hotmail.com>
2021-05-03 00:44:32 -0300

    menu dropdown adicionado

--- front/src/components/sidebar/Sidebar.tsx
+++ front/src/components/sidebar/Sidebar.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import icon from '../../assets/icon_pomodoro.png';
+import icon from '../../assets/icon_plus.png';
+import {SidebarData} from './SidebarData';
 import './Sidebar.css';
+import SubMenu from './SubMenu';
 
 function Sidebar() {
+
   return (
     <section className="sidebar">
-        <div>Side</div>
+        <a href=""><img className = "plusIcon" src={icon}/></a>
+        
+        <div className="sidebarLink">
+            {SidebarData.map((item, index) => {
+                return <SubMenu item={item} key={index} />;
+            })}
+        </div>
     </section>
   );
 }
