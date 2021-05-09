52c9e55ee90c49118050a0e98ec841413dba1a46
Bittarx <marcos10sb@gmail.com>
2021-05-08 21:52:33 -0300

    estilizando a página de criar lista

--- front/src/components/createlist/createlist.tsx
+++ front/src/components/createlist/createlist.tsx
@@ -4,18 +4,21 @@ import './createlist.css';
 function Createlist() {
   return (
     <div className="createlist">
-        <form method="" action="/">
-            <div>
+        <form className="form" method="" action="/">
+            <div className="titulo">
                 <p>Título:</p>
-                <input type="text"/>
+                <input type="text" className="titulo_input"/>
             </div>
             <div>
                 <p>Descrição:</p>
-                <textarea name="lala"></textarea>
+                <input type="text" className="descricao_input" ></input>
             </div>
             <div>
                 <p>Tarefas:</p>
-                <input type="text" name="" id="" />
+                <input type="text" className="tarefas_input"/>
+            </div>
+            <div>
+                <button type="button" className="savebutom" >Salvar</button> 
             </div>
         </form>
     </div>
