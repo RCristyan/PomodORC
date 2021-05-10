dc99ef1bb39d0e592f196aad06c0dcd0cea9e26a
Bittarx <marcos10sb@gmail.com>
2021-05-09 22:33:50 -0300

    finalizando a issue

--- front/src/components/createlist/createlist.tsx
+++ front/src/components/createlist/createlist.tsx
@@ -7,7 +7,6 @@ function Createlist() {
     <div className="createlist">
         <form className="form" method="" action="/">
             <div className="titulo">
-<<<<<<< HEAD
                 <h1>Título:</h1>
                 <input type="text" className="titulo_input"/>
             </div>
@@ -18,18 +17,6 @@ function Createlist() {
             <div className="tarefas">
                 <h1>Tarefas:</h1>
                 <input type="text" className="tarefas_input"/>
-=======
-                <p>Título:</p>
-                <input type="text" className="titulo_input" value="texto de referencia p/titulo"/>
-            </div>
-            <div>
-                <p>Descrição:</p>
-                <textarea className="descricao_input">texto de referencia p/descricao"</textarea>
-            </div>
-            <div>
-                <p>Tarefas:</p>
-                <input type="text" className="tarefas_input" value="Tarefa sendo criada"/>
->>>>>>> 0bc6e8b30ddb2544b679bd1f13d25c6d9140e112
             </div>
             <div>
                 <button type="button" className="savebutom" >Salvar</button> 
