df3b0cb7bac91a7d82bcb26eb290c268b9d69fcc
thyago <thyagohenriquemoura@gmail.com>
2021-05-10 11:00:24 -0300

    Adicionando novamente placeholders

--- front/src/components/createlist/createlist.tsx
+++ front/src/components/createlist/createlist.tsx
@@ -8,15 +8,16 @@ function Createlist() {
         <form className="form" method="" action="/">
             <div className="titulo">
                 <h1>Título:</h1>
-                <input type="text" className="titulo_input"/>
+                <input type="text" className="titulo_input" value="texto de referencia p/titulo"/>
             </div>
             <div className="descricao">
                 <h1>Descrição:</h1>
-                <input type="text" className="descricao_input" ></input>
+                <textarea className="descricao_input" value="texto de referencia p/descricao">   
+                </textarea>
             </div>
             <div className="tarefas">
                 <h1>Tarefas:</h1>
-                <input type="text" className="tarefas_input"/>
+                <input type="text" className="tarefas_input" value="titulo da tarefa"/>
             </div>
             <div>
                 <button type="button" className="savebutom" >Salvar</button> 
