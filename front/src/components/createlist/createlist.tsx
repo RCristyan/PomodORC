ee53dad57be8e7c72b835ab33afcd5e625f603da
Bittarx <marcos10sb@gmail.com>
2021-05-09 22:22:14 -0300

    finalizando a issue.

--- front/src/components/createlist/createlist.tsx
+++ front/src/components/createlist/createlist.tsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import './createlist.css';
+import icon from '../../assets/icon_black_plus.png';
 
 function Createlist() {
   return (
     <div className="createlist">
         <form className="form" method="" action="/">
             <div className="titulo">
-                <p>Título:</p>
+                <h1>Título:</h1>
                 <input type="text" className="titulo_input"/>
             </div>
-            <div>
-                <p>Descrição:</p>
+            <div className="descricao">
+                <h1>Descrição:</h1>
                 <input type="text" className="descricao_input" ></input>
             </div>
-            <div>
-                <p>Tarefas:</p>
+            <div className="tarefas">
+                <h1>Tarefas:</h1>
                 <input type="text" className="tarefas_input"/>
             </div>
             <div>
                 <button type="button" className="savebutom" >Salvar</button> 
+            </div>
+            <div className="black_plus">
+            <a href=""><img className = "plusIcon" src={icon}/></a>
             </div>
         </form>
     </div>
