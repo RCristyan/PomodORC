cd84bc9492d180fcd2c541307654b8a8e0e7f985
thyago <thyagohenriquemoura@gmail.com>
2021-05-18 14:51:02 -0300

    atualizacoes de pastas e rotas

---
+++ front/src/components/seelist/seelist.tsx
@@ -0,0 +1,16 @@
+import react from 'react';
+import './seelist.css';
+import {SidebarData} from '../sidebar/SidebarData'
+
+function Seelist(){
+    return(
+        <div className="seelist">
+            <section className="seelistcontainer">
+            <p>Lista 1</p>
+            <p>Descrição</p>
+            <p>texto de referencia p/ descricao</p>
+            </section>
+        </div>
+    )
+}
+export default Seelist;
---
+++ front/src/pages/seelistpage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Seelist from '../components/seelist/seelist';
+
+function SeeListPage() {
+  return (
+    <div className="seelist">
+      <Seelist />
+    </div>
+  );
+}
+
+export default SeeListPage;
