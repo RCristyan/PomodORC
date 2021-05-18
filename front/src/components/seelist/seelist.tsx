45d35dd6a8c07dc488e61514ad33b7ac2fa96e32
thyago <thyagohenriquemoura@gmail.com>
2021-05-18 18:51:05 -0300

    estilizacao da pagina de ver lista ate descricao

--- front/src/components/seelist/seelist.tsx
+++ front/src/components/seelist/seelist.tsx
@@ -13,23 +13,36 @@ function Seelist(props:any){
     return(
         <div className="seelist">
             <section className="seelistcontainer">
-                <div className="all-container">
-                    <div className="title-container">
-                        <div className="standardtitle">{title}</div>
-                    </div>
-                    <div className="icons-container">
-                        <div>
-                            <a href=""><img className="edit-icon" src={iconEdit} alt="Editar lista" /></a>
-                        </div>
-                        <div>
-                            <a href=""><img className="dump-icon" src={iconDump} alt="Deletar lista" /></a>
-                        </div>
-                    </div>
-                    <div className="description-container">
-                        <h1>Descricao</h1>
-                        <p>titulo referente p/ Descricao</p>
-                    </div>
-                </div>
+                        <section className="section-title-icon">
+                            {/* container do titulo */}
+                            <div className="title-container">
+                                <div className="standardtitle">{title}</div>
+                            </div>
+
+                            {/* conteiner dos icones */}
+                            <div className="icons-container">
+
+                                {/* icone de edicao */}
+                                <div className="container-edit-icon">
+                                    <a href=""><img className="edit-icon" src={iconEdit} alt="Editar lista" /></a>
+                                </div>
+
+                                {/* icone de lixeira */}
+                                <div className="container-dump-icon">
+                                    <a href=""><img className="dump-icon" src={iconDump} alt="Deletar lista" /></a>
+                                </div>
+                            </div>
+                        </section>
+                        <section className="section-description">
+                            {/* descricao */}
+                            <div className="description-container">
+                                <h1>Descricao</h1>
+                                <p>titulo referente p/ Descricao</p>
+                            </div>
+                        </section>
+
+
+
                 {/* menu de tarefas */}
                 {/* <ListarTarefas item={Seelistdata}/> */}
             </section>
