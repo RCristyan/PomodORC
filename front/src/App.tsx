7066f8e33b3e11e6f31fa9627ccefb531c7beb90
thyago <thyagohenriquemoura@gmail.com>
2021-05-20 20:05:31 -0300

    mudanças

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,15 @@
-<<<<<<< HEAD
-=======
 import Routes from "./router";
->>>>>>> e3df1d4dd6ddeefc5ef414b4680be7a22522a603
 import './App.css';
 import Router from './router';
 
-<<<<<<< HEAD
-=======
 
 ///
 ///<StopWatch listTitle="Lista1" activityTitle="Tarefa1"/>
->>>>>>> e3df1d4dd6ddeefc5ef414b4680be7a22522a603
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-        <Router />
-=======
-      <Routes />
->>>>>>> e3df1d4dd6ddeefc5ef414b4680be7a22522a603
+      <Router />
     </div>
   );
 }
