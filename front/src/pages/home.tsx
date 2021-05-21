79c9b05d342f644405e56f52ac88497821d6ef1e
João Victor Batista <jvtexbat@hotmail.com>
2021-05-20 23:23:38 -0300

    cronometro ajustado na home

--- front/src/pages/home.tsx
+++ front/src/pages/home.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Timer from "../components/timer/timer";
+import StopWatch from "../components/timer/StopWatch";
 
 function Home(props:any) {
-  let time = props.match.params.totaltime===undefined ? 0: props.match.params.totaltime;
+  let time = (props.match.params.totaltime===undefined) ? 0: props.match.params.totaltime;
+  let crono = props.match.params.totaltime==="crono" ? true: false;
   let list = props.match.params.list;
   let activity = props.match.params.activity===undefined ? "Bem-Vindo!!!": props.match.params.activity;
 
   return (
     <div className="Home">
-      <Timer listTitle={list} activityTitle={activity} totaltime={time}/>
+      {!crono && <Timer listTitle={list} activityTitle={activity} totaltime={time}/>}
+      {crono && <StopWatch listTitle={list} activityTitle={activity}/>}
     </div>
   );
 }
